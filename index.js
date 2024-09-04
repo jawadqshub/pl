869e71a35df52a575205b4a659db768d5c313b6a
jawadqshub <107557229+jawadqshub@users.noreply.github.com>
2024-09-04 13:10:18 +0500

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Define your jQuery function
+function callJQueryFunction(url) {
+    // jQuery code here
+    console.log("Hello from jQuery!");
+    window.location.href = url;
+}
