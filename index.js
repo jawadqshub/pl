dc0930d672a52d235149284de6fd26ed8dfffbce
jawadqshub <jawad_qshub@hotmail.com>
2024-09-10 12:13:57 +0500

    New Sample Added by deleting old code and adding new
    New code added. Old deleted

--- index.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// Define your jQuery function
-function callJQueryFunction(url) {
-    // jQuery code here
-    console.log("Hello from jQuery!");
-    window.location.href = url;
-}
